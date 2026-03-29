fd383582b5282922c2e5ed7a84b1d263362ca043
NivBar2000 <bar.niv2000@gmail.com>
2026-03-29 21:49:38 +0300

    finished practice for lap

---
+++ newapp/loops/Dictionaries_Practice.py
@@ -0,0 +1,14 @@
+friend_ages = {
+    "Rolf": 24,
+    "Adam": 30,
+    "Anne": 27,
+    "Bob" : 20
+}
+
+# friend_ages["Rolf"] = 40
+# print(friend_ages)
+
+# if "Bob" in friend_ages:
+#    print("bob attended")
+
+print(friend_ages[0])
--- newapp/loops/practice_forloops.py
+++ newapp/loops/practice_forloops.py
@@ -39,3 +39,61 @@
 
 # mylis=["dani", "dona", "coca"]
 # print(len(mylis))
+
+#====ex11====
+
+mylis=["danny", "roni", "dan", "Avi", "Alon"]
+# for name in mylis:
+#     print(name)
+
+#====ex12====
+
+# print(len(mylis))
+
+#=====ex13====
+numlis=[43,1,42,5,6,-4,-78,-13,-4]
+# print(sum(numlis))
+
+#=====ex14====
+
+# print(max(numlis))
+
+#=====ex15====
+
+# for name in mylis:
+#     if name.startswith("A"):
+#         print(name)
+
+#=====ex16====
+
+# for num in numlis:
+#     if num % 2 == 0:
+#         print(num)
+
+#=====ex17====
+
+counter=0
+for num in numlis:
+    if num > 10:
+        counter+=1
+#print(counter)
+
+#=====ex18====
+
+# for num in numlis:
+#     if num % 2 == 0 and num % 3 == 0:
+#         print(num)
+
+#=====ex19====
+
+# for num in numlis:
+#     print(num*num)
+
+#======ex20===
+
+last_list=[]
+
+for num in numlis:
+    if num > 0:
+        last_list.append(num)
+#print(last_list)
