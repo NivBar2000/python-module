86838968e195fe47369fca7ee6b7d0fe61bc7127
NivBar2000 <bar.niv2000@gmail.com>
2026-03-29 14:19:46 +0300

    learned advenced loops and done some practice

---
+++ newapp/loops/Enumerate&Zip.py
@@ -0,0 +1,30 @@
+#ex1:
+
+names = ["Alice", "Bob", "Charlie"]
+
+# for name in enumerate(names):
+#     print(name)
+
+#ex2:
+
+servers = ["app-01", "app-02", "app-03"]
+# for server in enumerate(servers, start=1):
+#     print(server)
+
+#ex3:
+
+errors = ["disk full", "timeout", "connection lost"]
+
+# for erroe in enumerate(errors):
+#     if erroe[0]%2==0:
+#         print(erroe)
+
+#ex4:
+
+ports = [22, 80, 443, 8080]
+# for port in enumerate(ports):
+#     if port[0] > 1:
+#         print(port)
+
+#ex5:
+
---
+++ newapp/loops/practice_forloops.py
@@ -0,0 +1,41 @@
+#====ex1=====
+# for  i in range (1,6):
+#     print(i)
+
+#======ex2====
+    
+# for  i in range (5,0,-1):
+ #   print(i)
+ 
+#====ex3====
+
+# for i in range (1,6):
+#     if  i % 2==0:
+#         print (i)
+
+#====ex4====
+
+# for  i in range (1,11):
+#     print(i)
+
+#=====ex5===
+
+# for i in range (0,21,3):
+#     print(i)
+
+#====ex6====
+
+# for hello in range (5):
+#  print ("hello")
+
+#====ex7=====
+
+# sum=0
+# for i in range (0,101):
+#     sum +=i
+# print (sum)
+
+#===ex8======
+
+# mylis=["dani", "dona", "coca"]
+# print(len(mylis))
